cabb1683ed2e18a5ab1a1becc8d136fd7758fec9
farhan 27007 <muhammadfarhan27007@gmail.com>
2020-12-02 20:20:09 +0700

    penambahan page laporan IKU

---
+++ cetak-laporan-iku.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Edit user - Emonev Dashboard</title>
+	<meta name="description" content="Marketing Dashboard">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+	<!-- Call App Mode on ios devices -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<!-- Remove Tap Highlight on Windows Phone IE -->
+	<meta name="msapplication-tap-highlight" content="no">
+	<!-- base css -->
+	<link rel="stylesheet" media="screen, print" href="dist/css/vendors.bundle.css">
+	<link rel="stylesheet" media="screen, print" href="dist/css/app.bundle.css">
+</head>
+
+<body>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-3">
+				<p class="text-center">
+					<img src="dist/img/logo-lamsel.png" class="img-fluid img-kop-surat" alt="">
+				</p>
+			</div>
+			<div class="col-9 text-center t-font">
+				<h2 class="font-weight-bold">PEMERINTAH DAERAH KABUPATEN LAMPUNG SELATAN</h2>
+				<h3 class="font-weight-bold">BAPPEDA KABUPATEN LAMPUNG SELATAN</h3>
+				<p class="mb-2">Jl. Kolonel Makmum Rasyid No.16, Kalianda</p>
+				<h2 class="font-weight-bold lts-4">KALIANDA</h2>
+			</div>
+			<div class="bb-1 mt-1 w-100"></div>
+			<div class="bb-3 mt-2 w-100"></div>
+
+			<table class="table table-bordered mt-4">
+				<thead>
+					<tr align="center">
+						<th>Kode Indikator</th>
+						<th>Indikator</th>
+						<th>Indikator Angka</th>
+						<th>OPD</th>
+						<th>Tahun</th>
+						<th>Keterangan</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr align="center">
+						<th>1</th>
+						<td>Hasil survei indeks kegunaan masyarakat</td>
+						<td>10.15 %</td>
+						<td>Dinas Perhubungan</td>
+						<td>2008</td>
+						<td>-</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>	
+	</div>
+
+</body>
+</html>
+
+<script src="dist/js/vendors.bundle.js"></script>
+<script src="dist/js/app.bundle.js"></script>
+<script type="text/javascript" src="dist/js/set-print-page.js"></script>
+
+<script type="text/javascript">
+	/* Activate smart panels */
+	$('#js-page-content').smartPanel();
+</script>
+
+<style>
+@media screen, print {
+	.t-font {
+		font-family: times new roman;
+	}
+	.img-kop-surat {
+		height: 3.2cm;
+		width: 2.63cm;
+	}
+	.lts-4 {
+		letter-spacing: 4px;
+	}
+	.bb-1 {
+		border-bottom: 1px solid #000;
+	}
+	.bb-3 {
+		border-bottom: 3px solid #000;
+	}
+}
+</style>
