b2728a1b7c9cf4974d5734fef37e28b8b6480eb5
farhan 27007 <muhammadfarhan27007@gmail.com>
2020-12-16 22:01:08 +0700

    penambahan cetak laporan IKU BARU

---
+++ cetak-lap-iku-new.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Cetak Laporan IKU Baru - Emonev Dashboard</title>
+	<meta name="description" content="Marketing Dashboard">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+	<!-- Call App Mode on ios devices -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<!-- Remove Tap Highlight on Windows Phone IE -->
+	<meta name="msapplication-tap-highlight" content="no">
+</head>
+<body>
+	
+	<h3>BAB IX RPJMD  DARI PERANGKAT DAERAH  TAHUN 2020</h3>
+	<table cellspacing="0" border="0">
+
+		<colgroup width="64"></colgroup>
+		<colgroup width="66"></colgroup>
+		<colgroup width="64"></colgroup>
+		<colgroup width="149"></colgroup>
+		<colgroup width="110"></colgroup>
+		<colgroup width="114"></colgroup>
+		<colgroup span="12" width="64"></colgroup>
+		<colgroup width="121"></colgroup>
+		<colgroup width="140"></colgroup>
+
+		<tr>
+			<td class="set-table-iku grey-bg" colspan=4 rowspan=3 height="128" align="center" valign=middle>
+				<b>Aspek/Fokus/Bidang Urusan/ Indikator Kinerja "Pembangunan Daerah"</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=middle>
+				<b>Kondisi Kinerja pada awal periode RPJMD</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=middle>
+				<b>Kondisi Kinerja pada Akhir periode RPJMD</b>
+			</td>
+			<td class="set-table-iku grey-bg" colspan=12 align="center" valign=middle>
+				<b>Target  dan Capaian Setiap Tahun</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=middle>
+				<b>CAPAIAN TERHADAP AKHIR RPJMD</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=middle>
+				<b>Akan Tercapai, Tercapai,Perlu Usaha Keras </b>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="set-table-iku grey-bg" align="center" valign=top><b><br></b></td>
+			<td class="set-table-iku grey-bg" align="left" valign=top><br></td>
+			<td class="set-table-iku grey-bg" colspan=4 align="center" valign=middle><b>TARGET</b></td>
+			<td class="set-table-iku grey-bg" colspan=4 align="center" valign=middle><b>REALISASI</b></td>
+			<td class="set-table-iku grey-bg" colspan=4 align="center" valign=middle ><b>CAPAIAN</b></td>
+			<td class="set-table-iku grey-bg" align="left" valign=middle><b><br></b></td>
+			<td class="set-table-iku grey-bg" align="left" valign=middle bgcolor="#BFBFBF"><br></td>
+		</tr>
+
+		<tr>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2014</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2021</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2016</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2017</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2018</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2019</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2016</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2017</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2018</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2019</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2016</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2017</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2018</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="center" valign=top>
+				<b>2019</b>
+			</td>
+			<td class="set-table-iku grey-bg" align="left" valign=middle><b><br></b></td>
+			<td class="set-table-iku grey-bg" align="left" valign=middle><br></td>
+		</tr>
+
+		<tr>
+			<td class="set-table-iku" height="20" align="left" valign=middle><b>1.1.</b></td>
+			<td class="set-table-iku" colspan=3 align="left" valign=middle><b>ASPEK KESEJAHTERAAN MASYARAKAT</b></td>
+			<td class="set-table-iku" align="center" valign=middle><br></td>
+			<td class="set-table-iku" align="center" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+			<td class="set-table-iku" align="left" valign=middle><b><br></b></td>
+			<td class="set-table-iku" align="center" valign=middle><b><br></b></td>
+			<td class="set-table-iku" align="left" valign=middle class="set-table-iku"><br></td>
+		</tr>
+
+		<tr>
+			<td class="set-table-iku" height="20" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><b>1.1.1.</b></td>
+			<td class="set-table-iku" colspan=2 align="left" valign=middle><b>Fokus Kesejahteraan Dan Pemerataan Ekonomi</b></td>
+			<td class="set-table-iku" align="center" valign=middle><br></td>
+			<td class="set-table-iku" align="center" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><b><br></b></td>
+			<td class="set-table-iku" align="center" valign=middle><b><br></b></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+		</tr>
+		<tr>
+			<td class="set-table-iku" height="20" align="left" valign=middle><br></td>
+			<td class="set-table-iku" align="left" valign=middle><br></td>
+			<td class="set-table-iku" colspan=2 align="left" valign=middle>1. Pertumbuhan Ekonomi (PDRB) (%)</td>
+			<td class="set-table-iku" align="center" valign=middle>6</td>
+			<td class="set-table-iku" align="center" valign=middle>6.6</td>
+			<td class="set-table-iku" align="center" valign=middle>6.17</td>
+			<td class="set-table-iku" align="center" valign=middle>6.2</td>
+			<td class="set-table-iku" align="center" valign=middle>6.3</td>
+			<td class="set-table-iku" align="center" valign=middle>6.4</td>
+			<td class="set-table-iku" align="center" valign=middle>5.22</td>
+			<td class="set-table-iku" align="center" valign=middle>5.46</td>
+			<td class="set-table-iku" align="center" valign=middle>5.26</td>
+			<td class="set-table-iku" align="center" valign=middle>5.14</td>
+			<td class="set-table-iku" align="center" valign=middle>84.60%</td>
+			<td class="set-table-iku" align="center" valign=middle>111.57%</td>
+			<td class="set-table-iku" align="center" valign=middle>83.49%</td>
+			<td class="set-table-iku" align="center" valign=middle><b>80.31%</b></td>
+			<td class="set-table-iku" align="center" valign=middle>83%</td>
+			<td class="set-table-iku" align="center" valign=middle>Akan Tercapai</td>
+		</tr>
+
+		<tr>
+			<td class="set-table-iku" height="20" align="left" valign=middle bgcolor="#92D050"><br></td>
+			<td class="set-table-iku" align="left" valign=middle bgcolor="#92D050"><br></td>
+			<td class="set-table-iku" colspan=2 align="left" valign=middle bgcolor="#92D050"><b>RATA-RATA CAPAIAN KINERJA</b></td>
+			<td class="set-table-iku" align="center" valign=middle bgcolor="#92D050"><b><br></b></td>
+			<td class="set-table-iku" align="center" valign=middle bgcolor="#92D050"><br></td>
+			<td class="set-table-iku" align="center" valign=middle bgcolor="#92D050"><font face="Cambria" size=3>	<br>
+			</td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><br></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><br></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><br></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b><br></b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b><br></b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b><br></b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b><br></b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b>6%</b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b>8%</b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b>6%</b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle ><b>6%</b></td>
+			<td class="set-table-iku green-bg" align="center" valign=middle><b>6%</b></td>
+			<td class="set-table-iku green-bg" align="left" valign=middle><br></td>
+		</tr>
+
+		<tr>
+			<td height="20" align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+			<td align="left" valign=bottom><br></td>
+		</tr>
+	</table>
+
+	<p>Tercapai : 34</p>
+	<p>Akan Tercapai : 29</p>
+	<p>Perlu Usaha Keras : 9</p>
+	<!-- ************************************************************************** -->
+
+</body>
+</html>
+
+<script src="dist/js/vendors.bundle.js"></script>
+<script src="dist/js/app.bundle.js"></script>
+<script type="text/javascript" src="dist/js/set-print-page.js"></script>
+
+<script type="text/javascript">
+	/* Activate smart panels */
+	$('#js-page-content').smartPanel();
+</script>
+
+<style type="text/css">
+
+.set-table-iku {
+	font-size: x-small;
+	border: 1px solid #000;
+	font-family: cambria;
+}
+.grey-bg {
+	background:#bfbfbf;
+}
+.green-bg {
+	background: #92D050;
+}
+
+</style>
